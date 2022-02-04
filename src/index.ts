ac4e0057f1e0da0d516980f7ad86898502374aba
Octanium <octaniumcontact@gmail.com>
2022-02-04 12:31:04 +0100

    :heavy_plus_sign:  add mongoose

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,37 @@ import 'dotenv/config';
 
 import { app } from './app';
 import * as http from 'http';
+import mongoose from 'mongoose';
 
-function normalizePort(val: string) {
-    const port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        // named pipe
-        return val;
+(async () => {
+    if (!process.env.DB_URI) {
+        throw new Error('DB_URI environment variable must be set.');
     }
 
-    if (port >= 0) {
-        // port number
-        return port;
+    // Connect to MongoDB
+    await mongoose.connect(process.env.DB_URI);
+
+    // Initialize HTTP server
+    function normalizePort(val: string) {
+        const port = parseInt(val, 10);
+
+        if (isNaN(port)) {
+            // named pipe
+            return val;
+        }
+
+        if (port >= 0) {
+            // port number
+            return port;
+        }
+
+        return false;
     }
 
-    return false;
-}
+    const port = normalizePort(process.env.PORT || '3000');
+    const server = http.createServer(app);
 
-const port = normalizePort(process.env.PORT || '3000');
-const server = http.createServer(app);
-
-server.listen(port, () => {
-    console.log(`🚀 Hot Take API listening on port ${port} !`);
-});
+    server.listen(port, () => {
+        console.log(`🚀 Hot Take API listening on port ${port} !`);
+    });
+})();
