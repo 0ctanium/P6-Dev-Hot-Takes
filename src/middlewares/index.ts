af678068ba3d86425243296eccdb84cb4e67bd9e
Octanium <octaniumcontact@gmail.com>
2022-02-05 22:53:45 +0100

    :sparkles:  add auth middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,1 +1,9 @@
+import { TOKEN_SECRET } from '../config';
+
 export * from './morgan';
+import jwt from 'express-jwt';
+
+export const requireAuth = jwt({
+    secret: TOKEN_SECRET,
+    algorithms: ['HS256'],
+});
